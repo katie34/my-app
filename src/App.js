362c0108338f87796805a4b5734c78b50c1bf7cc
katie34 <89612931+katie34@users.noreply.github.com>
2021-11-14 15:32:24 -0500

    adding starter code to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './components/Home';
+import AccountBalance from './AccountBalance';
+    
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      accountBalance: 14568.27
+    }
+  } 
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          Hello, World!
+        </div>
+      </Router>
+    );
+  }
+}
+//export default App;
+
+// src/components/Home.js
+class Home extends Component {
+  render() {
+    return (
+        <div>
+          <img src="https://i.picsum.photos/id/408/201/201.jpg?hmac=G2DODrvjG9jMtGHJh--DOFmtsQBExiUgINpVR0ARad8" alt="bank"/>
+          <h1>Bank of React</h1>
+        </div>
+    );
+  }
+}
+export default Home;
+
+class AccountBalance extends Component {
+  render() {
+    return (
+      <div>
+        Balance: {this.props.accountBalance}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default AccountBalance;
+
+render (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={<Home accountBalance={this.state.accountBalance}/>}/>
+    </Switch>
+  </Router>
+)
+
