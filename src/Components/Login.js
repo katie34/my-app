2b33740dff20c458877dad6e19147764622b9057
katie34 <89612931+katie34@users.noreply.github.com>
2021-11-15 13:47:31 -0500

    adding login component

---
+++ src/Components/Login.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import {Link} from 'react-router-dom';
+
+class LogIn extends Component {
+  constructor () {
+    super()
+    this.state = {
+      user: {
+        userName: '',
+        password: ''
+      },
+      redirect: false
+    }
+  }
+
+  handleChange = (e) => {
+    const updatedUser = {...this.state.user}
+    const inputField = e.target.name
+    const inputValue = e.target.value
+    updatedUser[inputField] = inputValue
+
+    this.setState({user: updatedUser})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.mockLogIn(this.state.user)
+    this.setState({redirect: true})
+  }
+
+  render () {
+    if (this.state.redirect) {
+      return (<Redirect to="/userProfile"/>)
+    }
+
+    return (
+
+      <div className ="container d-flex justify-content-center align-itens-center">
+       <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="userName">User Name</label>
+            <input type="text" name="userName" onChange={this.handleChange} value={this.state.user.userName} />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" />
+          </div>
+          <button className ="btn btn-danger mr-2">Log In</button>
+          <Link className ="btn btn-danger" to='/'>Home</Link>
+        </form>
+        </div>
+      
+      </div>
+    )
+  }
+}
+
+export default LogIn
