e0c2dfb89a9f91726d4c4615c48ad6d30ae53493
katie34 <89612931+katie34@users.noreply.github.com>
2021-11-15 13:39:43 -0500

    updating home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,22 @@ import {Link} from 'react-router-dom';
 class Home extends Component {
     render() {
         return (
-
+            <div className="container">
+            <div className = "d-flex justify-content-center align-items-center">
+            </div>
+            <div className = "d-flex">
+                <img src="https://static.thenounproject.com/png/91988-200.png" alt="bank"
+                style={{ width: 50, height: 50 }}/>
+                <h1>Bank of React</h1>
+                </div>
+                <div className = "mt-2">
+                <Link className ="btn btn-danger" to="/login">Login</Link>
+                <Link className ="btn btn-danger ml-1" to="/userProfile">User Profile</Link>
+                <Link className ="btn btn-danger ml-1" to="/Debits">Debits</Link>
+                <Link className ="btn btn-danger ml-1" to="/Credits">Credits</Link>
+                </div>
+                <AccountBalance accountBalance = {this.props.accountBalance}/>
+                </div>
         );
     }
 }
