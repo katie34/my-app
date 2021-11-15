c2e8cbe408847237e673d845e31ecefcc828c599
katie34 <89612931+katie34@users.noreply.github.com>
2021-11-15 13:21:56 -0500

    adding home component file

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import AccountBalance from './AccountBlance';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Home;
